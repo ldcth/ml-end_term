7536c31bb847f80afabcd2a7743d879137e690b2
Jason Le <leductho263@gmail.com>
2023-12-16 12:51:16 +0700

    save training

--- models/text/nb.py
+++ models/text/nb.py
@@ -1,5 +1,6 @@
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import joblib
 
 from sklearn.naive_bayes import MultinomialNB
 
@@ -9,11 +10,16 @@ def multinomialNaiveBayes(x_train, x_test, y_train, y_test):
 
     y_pred = classifier.predict(x_test)
 
+    joblib.dump(classifier, './checkpoint/nb_classifier.joblib')
+
     # Accuracy
     accuracy = accuracy_score(y_test, y_pred)
     # print(f"Accuracy: {accuracy}")
-
+ 
     # Confusion Matrix
     cm = confusion_matrix(y_test, y_pred)
 
     return accuracy, cm
+
+def a():
+    return 1
--- models/text/svm.py
+++ models/text/svm.py
@@ -1,5 +1,6 @@
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import joblib
 
 from sklearn.svm import SVC
 def svm(x_train, x_test, y_train, y_test):
@@ -15,6 +16,41 @@ def svm(x_train, x_test, y_train, y_test):
     # print(f"Accuracy: {accuracy}")
 
     # Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    return accuracy, cm
+
+def svm_rbf(x_train, x_test, y_train, y_test):
+    classifier =  SVC(kernel="rbf")
+    # classifier = SVC()
+
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+
+    # Accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    # print(f"Accuracy: {accuracy}")
+
+    # Confusion Matrix
+    cm = confusion_matrix(y_test, y_pred)
+
+    return accuracy, cm
+
+def svm_rbf_c(x_train, x_test, y_train, y_test):
+    classifier =  SVC(kernel="rbf", C=1000)
+
+    classifier.fit(x_train, y_train)
+
+    y_pred = classifier.predict(x_test)
+
+    joblib.dump(classifier, './checkpoint/svm_rbf_c_classifier.joblib')
+
+    # Accuracy
+    accuracy = accuracy_score(y_test, y_pred)
+    # print(f"Accuracy: {accuracy}")
+
+    # Confusion Matrixs
     cm = confusion_matrix(y_test, y_pred)
 
     return accuracy, cm
